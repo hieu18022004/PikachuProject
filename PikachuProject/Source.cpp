f4868bdf959e10a08c14cc195c8172fd0f7b958e
hieu18022004 <hieu180230@gmail.com>
2023-03-23 13:29:03 +0700

    Update Source.cpp

--- PikachuProject/Source.cpp
+++ PikachuProject/Source.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-	HWND hWnd = GetConsoleWindow();
+	HWND hWnd = GetConsoleWindow(); //console full screen ref: https://www.daniweb.com/programming/software-development/threads/31665/how-to-make-console-programs-full-screen.
 	ShowWindow(hWnd, SW_SHOWMAXIMIZED);
 	gameInit("Hieu", 0, 0);
 	return 1;
